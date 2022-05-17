cf337cdc413d90c4f129526000c18efce16083e6
Ketamin <you@example.com>
2022-05-17 10:58:33 +0700

    setup

--- src/config/translation.js
+++ src/config/translation.js
@@ -2,10 +2,12 @@ const languageData = {
   /************ ENGLISH **************/
   en: {
     address: "Ocean Park Building - No.1 Dao Duy Anh",
+    name:"nam"
   },
   /************ VietNam **************/
   vi: {
     address: "Số 1 - Đào Duy Anh - Hà Nội",
+    name:"alex"
   },
 };
 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,6 +2,7 @@ function Home({ languages }) {
   return (
     <>
       <h1>{languages.address}</h1>
+      <h1>{languages.name}</h1>
     </>
   );
 }
