1d594f80b23c5d8b2a50359d26acd5f1aaff4ed7
luyennk <luyennk@smartten.com.vn>
2022-05-24 20:22:31 +0700

    update

--- src/routers/index.js
+++ src/routers/index.js
@@ -1,41 +1,9 @@
 import Home from "../pages/Home";
-import AboutUs from "../pages/AboutUs";
-import Blog from "../pages/Blog";
-import Post from "../pages/BlogPost";
-import Services from "../pages/Services";
-import ServicePost from "../pages/ServicesPost";
-import Career from "../pages/Career"
 
 const publicRouter = [
   {
     path: "/",
     component: Home,
-  },
-  {
-    path:"/about-us",
-    component: AboutUs,
-  },
-  {
-    path:"/blog",
-    component: Blog,
-  },
-  {
-    path:"/blog/post/:blogIndex",
-    component:Post,
-  },
-  {
-    path:"/services",
-    component:Services,
-  },
-  {
-    path:"/services/post",
-    component: ServicePost,
-
-  },
-  {
-    path:"/career",
-    component: Career,
-
   }
   
 ];
